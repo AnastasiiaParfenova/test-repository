de3ab0b333b322bc981f9b8b6f34cc1115e22c85
Anastasiia Parfenova <aap1612@yandex.ru>
2018-07-26 17:59:58 +0500

    Добавлено решение задания 11 - сценарий для регистрации нового пользователя.

--- Selenium/java-example/src/test/java/ru/stqa/training/selenium/Task11.java
+++ Selenium/java-example/src/test/java/ru/stqa/training/selenium/Task11.java
@@ -1,41 +1,143 @@
 package ru.stqa.training.selenium;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.Select;
+import java.util.*;
+import java.lang.String;
 
-import static org.junit.Assert.assertTrue;
+
 
 public class Task11 extends TestBase {
 
-    @Test
-    public void Test() {
-        driver.get("http://localhost/litecart/en/");
-        driver.findElement(By.linkText("New customers click here")).click();
-        WebElement taxId = driver.findElement(By.name("tax_id"));
-        taxId.sendKeys("12345");
-        WebElement company = driver.findElement(By.name("company"));
-        company.sendKeys("Test");
-        WebElement firstname = driver.findElement(By.name("firstname"));
-        firstname.sendKeys("Test");
-        WebElement lastname = driver.findElement(By.name("lastname"));
-        lastname.sendKeys("Test");
-        WebElement address1 = driver.findElement(By.name("address1"));
-        address1.sendKeys("Test");
-        WebElement address2 = driver.findElement(By.name("address2"));
-        address2.sendKeys("Test");
-        WebElement postcode = driver.findElement(By.name("postcode"));
-        postcode.sendKeys("12345");
-        WebElement city = driver.findElement(By.name("city"));
-        city.sendKeys("Test");
-        WebElement email = driver.findElement(By.cssSelector("main#content")).findElement(By.name("email"));
-        email.sendKeys("Test@Test.com");
-        WebElement phone = driver.findElement(By.name("phone"));
-        phone.sendKeys("89000000000");
-        WebElement password = driver.findElement(By.cssSelector("main#content")).findElement(By.name("password"));
-        password.sendKeys("test1");
-        WebElement confirmedPassword = driver.findElement(By.name("confirmed_password"));
-        confirmedPassword.sendKeys("test1");
+
+    /**
+     * Ввод значения в поле ввода.
+     * @param searchContext контекст поиска.
+     * @param name имя поля
+     * @param text текст для ввода
+     */
+    public void setInputField(WebElement searchContext, String name, String text){
+        try {
+            WebElement inputField = searchContext.findElement(By.name(name));
+            inputField.clear();
+            inputField.sendKeys(text);
+        }
+        catch (NoSuchElementException e){
+            System.out.printf("Поле с именем %s не найдено \n", name);
+        }
     }
+
+    /**
+     * Выбор значения из выпадающего списка
+     * @param searchContext контекст поиска.
+     * @param fieldName имя поля
+     * @param selectValue значение для выбора из выпадающего списка
+     */
+    public void setSelect(WebElement searchContext, String fieldName, String selectValue){
+
+        try {
+            WebElement selectField = searchContext.findElement(By.name(fieldName));
+            Select select = new Select(selectField);
+            select.selectByValue(selectValue);
+        }
+        catch (NoSuchElementException e){
+            System.out.printf("Поле с именем %s не найдено \n", fieldName);
+        }
+    }
+
+    /**
+     * Генерация уникального пароля
+     */
+    public String generatePassword() {
+        return  UUID.randomUUID().toString();
+    }
+
+    /**
+     * Генерация уникального E-mail
+     */
+    public String generateEmail() {
+        return UUID.randomUUID().toString().concat("@test.ru");
+    }
+
+    /**
+     * Логин в клиентской части магазина
+     * @param email адрес электронной почты
+     * @param password пароль
+     */
+    public void login(String email, String password){
+
+        WebElement boxAccount = driver.findElement(By.id("box-account-login"));
+        setInputField(boxAccount, "email", email);
+        setInputField(boxAccount, "password", password);
+        try{
+            boxAccount.findElement(By.name("login")).click();
+        }
+        catch (NoSuchElementException e){
+            System.out.println("Кнопка login не найдена");
+        }
+    }
+
+    /**
+     * Выходи из учетной записи в клиентской части маагзина
+     */
+    public void logout(){
+        try{
+            driver.findElement(By.linkText("Logout")).click();
+        }
+        catch (NoSuchElementException e){
+            System.out.println("Кнопка Logout не найдена");
+        }
+    }
+
+    /**
+     * Создание новой учётной записи
+     * @param email адрес электронной почты
+     * @param password пароль
+     */
+    public void createAccount(String email, String password){
+        driver.get("http://localhost/litecart/en/");
+
+        try {
+            driver.findElement(By.linkText("New customers click here")).click();
+            WebElement searchContext = driver.findElement(By.cssSelector("main#content"));
+
+            setInputField(searchContext, "tax_id", "12345");
+            setInputField(searchContext, "company", "Test");
+            setInputField(searchContext, "firstname", "Test");
+            setInputField(searchContext, "lastname", "Test");
+            setInputField(searchContext, "address1", "Test");
+            setInputField(searchContext, "address2", "Test");
+            setInputField(searchContext, "postcode", "12345");
+            setInputField(searchContext, "city", "Test");
+            setSelect(searchContext, "country_code", "US");
+            setSelect(searchContext, "zone_code", "MA");
+            setInputField(searchContext, "email", email);
+            setInputField(searchContext, "phone", "+79000000000");
+            setInputField(searchContext, "password", password);
+            setInputField(searchContext, "confirmed_password", password);
+
+            driver.findElement(By.name("create_account")).click();
+        }
+        catch (NoSuchElementException e){
+            System.out.println(e.toString());
+        }
+
+    }
+
+    /**
+     * Создание новой учётной записи, выход, повторный вход в только что созданную учетную запись и выход
+     */
+    @Test
+    public void Test(){
+        String email = generateEmail();
+        String password = generatePassword();
+
+        createAccount(email, password);
+        logout();
+        login(email, password);
+        logout();
+    }
+
 }
